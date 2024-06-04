8450248c39f183405b9d48f1e4bb3fbbf7e2d94d
gudals123 <wnsals2111@naver.com>
2024-06-04 11:13:15 +0900

    주석 수제거
    스크립트 주석 제거

--- Assets/Scripts/Player/State/RollState.cs
+++ Assets/Scripts/Player/State/RollState.cs
@@ -11,22 +11,16 @@ public class RollState : StateMachineBehaviour
 
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
         _rigidbody = animator.GetComponentInParent<Rigidbody>();
 
         Vector3 rollDirection = new Vector3(animator.transform.localRotation.x, 0, animator.transform.localRotation.z);
 
         Roll(animator, rollDirection);
     }
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-
-    }
 
     public void Roll(Animator animator, Vector3 lastMoveDirection)
     {
         _rigidbody.velocity = lastMoveDirection + animator.transform.forward * dodgePower;
-        //_rigidbody.rotation = Quaternion.Euler(0, 0, 0);    
     }
 
 }
