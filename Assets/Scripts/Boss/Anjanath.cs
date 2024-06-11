67a461c3baa412a07c2b3d688990709951f1cd2b
havetodo-yeon <lotusdo216@gmail.com>
2024-06-11 23:44:25 +0900

    MON-145-Boss-ReDesign
    ## 설명
    
    1. Boss의 AI를 재설계하였습니다.
    
    2. Entity와 Controller 클래스 설계에 따라 계층 구조를 설계하고 하위 클래스들을 생성하였습
    니다.
    
    3. 우리 프로젝트의 Boss 이름은 "안자냐프"이므로 클래스의 이름을 Anjanath...로 시작하도록
    할 것입니다.
    
    4. 현재 대략적인 구조가 설계되었고, 그 구조에 따른 기본 메소드들을 생성해 놓은 상태입니다.
    
    5. 조정된 내용에 따라 보스의 트리 구조가 변경되었습니다.

---
+++ Assets/Scripts/Boss/Anjanath.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anjanath : Monster
+{
+    public State AnjanathState;
+    public Vector3 targetObjectPos;
+    public int DamageStack;
+
+    public bool startNormalAttaking;
+    public bool startBreathAttaking;
+
+    public bool isPlayerInAttackRange;
+    public bool isPlayerInVisualRange;
+
+    public float distancePtoB { get; private set; }
+    public bool _bossAttackRange { get; private set; }
+    public bool _bossVisualRange { get; private set; }
+    public bool _bossPerceptionRange { get; private set; }
+    public float _playerAttackDamege { get; private set; }
+
+
+
+    private void Awake()
+    {
+        grade = 1;
+        maxHp = 2000;
+        currentHp = maxHp;
+        rigidbody = GetComponent<Rigidbody>();
+        startPosition = rigidbody.position;
+        rotationSpeed = 100;
+        setHit = false;
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    public void SetAttackState()
+    {
+        if (currentHp <= 600 && SetChance())
+        {
+            Debug.Log("Breath@@@@@@@@@@@");
+            startBreathAttaking = true;
+            startNormalAttaking = false;
+        }
+        else
+        {
+            Debug.Log("Normal###########");
+            startBreathAttaking = false;
+            startNormalAttaking = true;
+        }
+    }
+
+    public void BreathAttacking()
+    {
+        attackDamage = 5;
+        animator.Play("BreathAttack");
+    }
+
+    public void NormalAttacking()
+    {
+        attackDamage = 10;
+        animator.Play("NormalAttack");
+    }
+
+    public override void SetDamage(int damage)
+    {
+        AnjanathState = State.SetDamage;
+
+        base.SetDamage(damage);
+
+        if(currentHp <= 0)
+        {
+            Dead();
+        }
+
+        else if(DamageStack == 5)
+        {
+            animator.Play("Sturn");
+        }
+
+        else if(DamageStack == 2)
+        {
+            animator.Play("Hit");
+        }
+    }
+
+    public IEnumerator AnjanathAttack(GameObject AttackObj, float duration)
+    {
+        canAttack = false;
+        AttackObj.SetActive(true);
+        yield return new WaitForSeconds(duration);
+        AttackObj.SetActive(false);
+        canAttack = true;
+    }
+
+    public bool SetChance()
+    {
+        return Random.Range(0f, 1.0f) <= 0.5f? true: false;
+    }
+
+
+    public void TrackingPlayer()
+    {
+        animator.Play("BattleTracking");
+        // GroundCheck 필요함
+        Move(5, targetObjectPos);
+    }
+
+    public void isPlayerInRange(Transform player, Transform boss)
+    {
+        distancePtoB = Vector3.Distance(player.position, boss.position);
+
+        Vector3 normalized = (player.position - boss.position).normalized;
+        float _isForward = Vector3.Dot(normalized, boss.forward);
+
+/*        // 인지 범위
+        if (distancePtoB <= 18f) _bossPerceptionRange = true;
+        else _bossPerceptionRange = false;
+*/
+        // 시야 범위
+        if (_isForward > 0 && distancePtoB <= 18f && distancePtoB >= 4f)
+        {
+            AnjanathState = State.Tracking;
+        }
+
+        // 공격 범위
+        if (_isForward > 0 && distancePtoB <= 7f)
+        {
+            isPlayerInAttackRange = true;
+        }
+    }
+
+}
