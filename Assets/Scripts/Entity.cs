bd74df00551934f16f21fde8fce518f24584029c
gudals123 <wnsals2111@naver.com>
2024-06-12 17:40:52 +0900

    MON-150 PlayerController1.cs, Test.cs파일 삭제
    #설명
    1. 현재 사용하지 않는 파일 삭제

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -6,8 +6,6 @@ abstract public class Entity : MonoBehaviour
 {
     public int maxHp { get; protected set; }
     public int currentHp { get; protected set; }
-    //public int attackDamage { get; protected set; }
-    //public Vector3 startPosition { get; protected set; }
     public Rigidbody rigidbody { get; protected set; }
     public Animator animator { get; protected set; }
 
