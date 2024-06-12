21509d0a5050d185bad89a57fc737d64826ee23a
havetodo-yeon <lotusdo216@gmail.com>
2024-06-12 10:41:49 +0900

    Weapon Class 생성
    공격 도구를 위한 Weapon Class 생성

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] protected GameObject _hitEffect;
+    protected GameObject hit;
+    protected float duration;
+    protected float attackDamage;
+
+    protected virtual void Awake()
+    {
+        hit = Instantiate(_hitEffect);
+        hit.SetActive(false);
+    }
+
+
+    protected virtual void OnTriggerEnter(Collider other) { }
+
+    protected virtual void AppearHitEffect(Vector3 hitPos, float duration)
+    {
+        StartCoroutine(CoHitEffect(hitPos, duration));
+    }
+
+    protected virtual IEnumerator CoHitEffect(Vector3 hitPos, float duration)
+    {
+        hit.transform.position = hitPos;
+        hit.SetActive(true);
+        yield return new WaitForSeconds(duration);
+        hit.SetActive(false);
+    }
+
+}
