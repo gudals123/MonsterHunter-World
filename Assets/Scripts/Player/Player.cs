2cc043b832bf27122df6a8ea228399ae9cbf1d3d
gudals123 <104682516+gudals123@users.noreply.github.com>
2024-06-16 11:13:40 +0900

    MON-174 부드러운 방향전환 구현
    #설명
    1. 기존 방식: characterBody.forward에 moveDir값을 넣어 한번에 이동 방향을 바라보도록 구현
    2. 현재 방식: moveDir값으로 목표 회전 방향을 구하고 Quaternion.Slerp을 현재 각도에서 목표 각도로
                 Time.deltaTime * moveSpeed만큼 나눠서 회전 하도록 수정 -> 결과: 부드럽게 회전하는 연출

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -89,7 +89,8 @@ public class Player : Entity
             Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
             Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
 
-            characterBody.forward = moveDir;
+            Quaternion targetRotation = Quaternion.LookRotation(moveDir);
+            characterBody.rotation = Quaternion.Slerp(characterBody.rotation, targetRotation, Time.deltaTime * moveSpeed);
 
             rigidbody.MovePosition(transform.position + moveDir * moveSpeed * Time.fixedDeltaTime);
         }
