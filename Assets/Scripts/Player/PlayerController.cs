27ea5928df0b7feb48bc646c40056095083c8a37
gudals123 <104682516+gudals123@users.noreply.github.com>
2024-06-17 01:10:39 +0900

    아이콘 추가, UIManager 퀵 슬롯 기능 추가
    #설명
    1. Potion, CatHeal, CatAttack 아이콘 추가
    2. UIManager.cs
     - SetIcon 함수를 통해 슬롯 아이콘을 설정한다.
     - UpdateQuickSlotIcon 함수를 통해 QuickSolt아이콘을 PlayerController.cs의 quickSlot과 동기화 했다.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -61,7 +61,7 @@ public class PlayerController : Controller
     private Skill_CatHeal catHeal;
 
     //Äü½½·Ô
-    private Slot[] QuickSlot;
+    private Slot[] quickSlot;
     private int quickSlotIndex = 0;
     private int quickSlotCount = 0;
 
@@ -78,11 +78,12 @@ public class PlayerController : Controller
         potion = new Item_Potion(player, 10, 10);
         catAttack = new Skill_CatAttack(cat);
         catHeal = new Skill_CatHeal(cat);
-        QuickSlot = new Slot[4];
-        QuickSlot[0] = potion;
-        QuickSlot[1] = catAttack;
-        QuickSlot[2] = catHeal;
+        quickSlot = new Slot[4];
+        quickSlot[0] = potion;
+        quickSlot[1] = catAttack;
+        quickSlot[2] = catHeal;
         quickSlotCount = 3;
+
     }
 
 
@@ -150,7 +151,7 @@ public class PlayerController : Controller
         }
         else if (Input.GetKeyDown(KeyCode.E) && !player.isArmed)
         {
-            QuickSlot[quickSlotIndex].Activate();
+            quickSlot[quickSlotIndex].Activate();
         }
         //Á¤Áö
         else
@@ -244,9 +245,10 @@ public class PlayerController : Controller
         }
         else if (scroll < 0f)
         {
-            quickSlotIndex = (quickSlotIndex + 1) % (quickSlotCount);
+            quickSlotIndex = (quickSlotIndex - 1 + quickSlotCount) % (quickSlotCount);
         }
-        //Debug.Log($"ÇöÀç Äü½½·Ô Index{quickSlotIndex}");
+        UIManager.Instance.UpdateQuickSlotIcon(quickSlot, quickSlotIndex, quickSlotCount);
+        Debug.Log($"ÇöÀç Äü½½·Ô Index{quickSlotIndex}");
     }
 
     public void StaminerRecovery()
--- Assets/Scripts/Slot/Slot.cs
+++ Assets/Scripts/Slot/Slot.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 abstract public class Slot
 {
+    public Image icon;
     abstract public void Activate();
 }
